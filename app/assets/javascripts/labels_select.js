120a462203e17b5414e47bb34165feb6d68f36af
黄涛 <htve@outlook.com>
2017-03-29 13:46:04 +0800

    标记列表中的汉化

--- app/assets/javascripts/labels_select.js
+++ app/assets/javascripts/labels_select.js
@@ -158,13 +158,13 @@
                 if (showNo) {
                   extraData.unshift({
                     id: 0,
-                    title: 'No Label'
+                    title: '无标记'
                   });
                 }
                 if (showAny) {
                   extraData.unshift({
                     isAny: true,
-                    title: 'Any Label'
+                    title: '任何标记'
                   });
                 }
                 if (extraData.length) {
