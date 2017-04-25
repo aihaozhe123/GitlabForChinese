e6d459aae6c092d7a62e51485ad538609a427bfc
黄涛 <htve@outlook.com>
2017-04-25 16:54:20 +0800

    汉化日期选择控件

--- app/assets/javascripts/due_date_select.js
+++ app/assets/javascripts/due_date_select.js
@@ -48,6 +48,7 @@ class DueDateSelect {
       field: $dueDateInput.get(0),
       theme: 'gitlab-theme',
       format: 'yyyy-mm-dd',
+      i18n: {previousMonth:'上个月',nextMonth:'下个月',months:['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],weekdays:['周日','周一','周二','周三','周四','周五','周六'],weekdaysShort:['日','一','二','三','四','五','六']},
       onSelect: (dateText) => {
         const formattedDate = dateFormat(new Date(dateText), 'yyyy-mm-dd');
 
@@ -170,6 +171,7 @@ class DueDateSelectors {
         field: $datePicker.get(0),
         theme: 'gitlab-theme',
         format: 'yyyy-mm-dd',
+        i18n: {previousMonth:'上个月',nextMonth:'下个月',months:['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],weekdays:['周日','周一','周二','周三','周四','周五','周六'],weekdaysShort:['日','一','二','三','四','五','六']},
         onSelect(dateText) {
           $datePicker.val(dateFormat(new Date(dateText), 'yyyy-mm-dd'));
         }
